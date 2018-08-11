f3a8ad4f7df63caf798ddde143ed5a8add1abe9e
Thomas Inganni <thomas.inganni@ochinomichi.fr>
2018-08-11 14:52:02 +0200

    Ajout des fichiers de la classe PT6S3 au projet

---
+++ include/PT6S3.h
@@ -0,0 +1,16 @@
+#ifndef PT6S3_H
+#define PT6S3_H
+
+
+class PT6S3
+{
+    public:
+        PT6S3();
+        virtual ~PT6S3();
+
+    protected:
+
+    private:
+};
+
+#endif // PT6S3_H
---
+++ src/PT6S3.cpp
@@ -0,0 +1,11 @@
+#include "PT6S3.h"
+
+PT6S3::PT6S3()
+{
+    //ctor
+}
+
+PT6S3::~PT6S3()
+{
+    //dtor
+}
